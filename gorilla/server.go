1b1d198a72aec8febea733d1f30c290700ca486b
Abby <abbychau@gmail.com>
2019-12-09 10:47:04 +0900

    Update server.go

--- gorilla/server.go
+++ gorilla/server.go
@@ -3,20 +3,44 @@ package main
 import (
         "flag"
         "log"
-        "net/url"
-        "os"
-        "os/signal"
-        "time"
-        "strconv"
-        "sync"
-        "fmt"
+        "net/http"
+        //"time"
         "github.com/gorilla/websocket"
         "syscall"
+        "strings"
 )
 
 var addr = flag.String("addr", "ps2:8080", "http service address")
 
-func main(){
+var upgrader = websocket.Upgrader{} // use default options
+
+func echo(w http.ResponseWriter, r *http.Request) {
+        c, err := upgrader.Upgrade(w, r, nil)
+        if err != nil {
+                log.Print("upgrade:", err)
+                return
+        }
+        defer c.Close()
+        for {
+                mt, message, err := c.ReadMessage()
+                if err != nil {
+                        // log.Println("read:", err) // too noisy for closed connection
+                        break
+                }
+                //log.Printf("recv: %s", message)
+                for i:=0;i<10;i++ {
+
+                        err = c.WriteMessage(mt, message)
+                        if err != nil {
+                                log.Println("write:", err)
+                                err = c.WriteMessage(mt, []byte(strings.Repeat("0",1000)))
+
+                                break
+                        }
+                }
+        }
+}
+func main() {
         // Increase resources limitations
         var rLimit syscall.Rlimit
         if err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &rLimit); err != nil {
@@ -26,82 +50,9 @@ func main(){
         if err := syscall.Setrlimit(syscall.RLIMIT_NOFILE, &rLimit); err != nil {
                 panic(err)
         }
-        var wg sync.WaitGroup
-        for k:=0;k<2500;k++{
-                wg.Add(1)
-                go goZilla(&wg, k)
-        }
-        wg.Wait()
-}
-
-func goZilla(wg *sync.WaitGroup, id int) {
-        defer wg.Done()
-        fmt.Printf("Worker %v: Started\n", id)
         flag.Parse()
         log.SetFlags(0)
-
-        interrupt := make(chan os.Signal, 1)
-        signal.Notify(interrupt, os.Interrupt)
-
-        u := url.URL{Scheme: "ws", Host: *addr, Path: "/echo"}
-        log.Printf("connecting to %s", u.String())
-
-        c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
-        if err != nil {
-                log.Fatal("dial:", err)
-        }
-        defer c.Close()
-
-        done := make(chan struct{})
-        received := 0
-        go func() {
-                defer close(done)
-                for {
-                        _, _, err := c.ReadMessage()
-                        if err != nil {
-                                log.Println("read:", err)
-                                return
-                        }
-                        received += 1
-                        if received % 50 == 0 {
-                                //log.Printf("recv: %s", message)
-                                log.Printf("id:"+strconv.Itoa(id)+",t:"+strconv.Itoa(received))
-                        }
-                }
-        }()
-
-        ticker := time.NewTicker(time.Second)
-        defer ticker.Stop()
-
-        for {
-                select {
-                case <-done:
-                        return
-                case t := <-ticker.C:
-                        err := c.WriteMessage(websocket.TextMessage, []byte(t.String()))
-                        if err != nil {
-                                log.Println("write:", err)
-                                return
-                        }
-                        if received >= 20000 {
-                                return
-                        }
-                case <-interrupt:
-                        log.Println("interrupt")
-
-                        // Cleanly close the connection by sending a close message and then
-                        // waiting (with timeout) for the server to close the connection.
-                        err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
-                        if err != nil {
-                                log.Println("write close:", err)
-                                return
-                        }
-                        select {
-                        case <-done:
-                        case <-time.After(time.Second):
-                        }
-                        return
-                }
-        }
-        fmt.Printf("Worker %v: Finished\n", id)
+        http.HandleFunc("/echo", echo)
+        log.Fatal(http.ListenAndServe(*addr, nil))
 }
+
